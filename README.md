92eeccf688b53243ddb784e43162b3e158061d26
matsufriends <pikapikan0526@gmail.com>
2026-04-22 08:33:31 +0900

    chore: UPM 化を取りやめ、README を整備中に戻し package.json を削除
    feat: PlayerInput の join/leave を通知する OnPlayerInputsChanged static event を追加
    - 購読側 (MornUGUI 等) は毎 frame PlayerInput.all を走査せずに再キャッシュできる

--- README.md
+++ README.md
@@ -1,34 +1,3 @@
 # MornInput
 
-<p align="center">
-  <img src="src/Editor/MornInput.png" alt="MornInput" width="640" />
-</p>
-
-<p align="center">
-  <img src="https://img.shields.io/github/license/TsukumiStudio/MornInput" alt="License" />
-</p>
-
-## 概要
-
-Unity Input System のラッパー。マルチプレイヤー入力管理・スキーム切替検知・入力アイコン表示などを統一化する。
-
-## 導入方法
-
-Unity Package Manager で以下の Git URL を追加:
-
-```
-https://github.com/TsukumiStudio/MornInput.git?path=src#1.0.0
-```
-
-`Window > Package Manager > + > Add package from git URL...` に貼り付けてください。
-
-### 依存パッケージ
-
-- [Unity Input System](https://docs.unity3d.com/Packages/com.unity.inputsystem@latest) (`com.unity.inputsystem`)
-- [UniRx](https://github.com/neuecc/UniRx) (`com.neuecc.unirx`)
-- [VContainer](https://github.com/hadashiA/VContainer) (`jp.hadashikick.vcontainer`)
-- [MornGlobal](https://github.com/TsukumiStudio/MornGlobal) (`com.tsukumistudio.mornglobal`)
-
-## ライセンス
-
-[The Unlicense](LICENSE)
+整備中
--- src/MornInputProvider.cs
+++ src/MornInputProvider.cs
@@ -1,4 +1,5 @@
 ﻿#if USE_INPUTSYSTEM
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,6 +11,8 @@ namespace MornLib
         [SerializeField] private PlayerInputManager _playerInputManager;
         [SerializeField] private InputActionAsset _globalUIActions;
         private readonly Dictionary<PlayerInput, IMornInput> _inputs = new();
+        /// <summary>PlayerInput の join/leave で発火。購読側は登録中の最小 playerIndex 等を再計算する</summary>
+        public static event Action OnPlayerInputsChanged;
 
         private void Awake()
         {
@@ -40,13 +43,15 @@ namespace MornLib
             playerInput.transform.SetParent(transform);
             _inputs.Add(playerInput, new MornInputHandler(playerInput));
             Debug.Log($"Input added: index {playerInput.playerIndex}");
+            OnPlayerInputsChanged?.Invoke();
         }
-        
+
         private void OnPlayerLeft(PlayerInput playerInput)
         {
             if (_inputs.Remove(playerInput))
             {
                 Debug.Log($"Input removed: index {playerInput.playerIndex}");
+                OnPlayerInputsChanged?.Invoke();
             }
         }
 
